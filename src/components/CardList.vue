e5704517e6dc7f6e2893d995aa1b1731be934809
Johann <johann.dumoulin@asdoria.com>
2022-08-25 16:19:30 +0200

    Card listing

---
+++ src/components/CardList.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import TitleComponent from "./TitleComponent.vue";
+import CardItem from "./cardList/CardItem.vue";
+</script>
+
+<template>
+    <section>
+        <TitleComponent msg="Card List" />
+
+        <div class="container mx-auto">
+            <div class="flex flex-wrap -m-4">
+                <CardItem v-for="card in 7"/>
+            </div>
+        </div>
+
+    </section>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/cardList/CardItem.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="p-4 w-full sm:w-1/2 lg:w-1/3">
+        <div class="bg-fuchsia-50 h-100 border-3 border-black rounded-lg">
+
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
